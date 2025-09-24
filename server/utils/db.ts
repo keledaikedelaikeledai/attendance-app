3d4522eab0daa23627c7c3cb52871d7b7b4265ad
keledaikedelaikeledai <keledaikedelaikeledai@gmail.com>
2025-09-24 11:15:21 +0700

    fix: switch back to using drizzle

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -5,15 +5,15 @@ let _client: ReturnType<typeof createClient> | null = null
 let _db: ReturnType<typeof drizzle> | null = null
 
 export function useDb() {
-  // const client = createClient({
-  //   // eslint-disable-next-line node/prefer-global/process
-  //   url: process.env.NUXT_DB_URL as string,
-  //   // eslint-disable-next-line node/prefer-global/process
-  //   authToken: process.env.NUXT_DB_AUTH_TOKEN,
-  // })
   if (!_client) {
+    // _client = createClient({
+    //   url: 'file:./sqlite.db',
+    // })
     _client = createClient({
-      url: 'file:./sqlite.db',
+    // eslint-disable-next-line node/prefer-global/process
+      url: process.env.NUXT_DB_URL as string,
+      // eslint-disable-next-line node/prefer-global/process
+      authToken: process.env.NUXT_DB_AUTH_TOKEN,
     })
   }
 
