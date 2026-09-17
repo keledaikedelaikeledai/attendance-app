dd0df3a9eac38b5fc1dc7d97e12f772b4f46289e
keledaikedelaikeledai <keledaikedelaikeledai@gmail.com>
2026-09-17 13:45:02 +0700

    refactor(attendance): reuse shift validation helpers

--- server/api/attendance/clock-in.post.ts
+++ server/api/attendance/clock-in.post.ts
@@ -4,6 +4,7 @@ import { createError, readBody } from 'h3'
 import { attendanceDay, attendanceLog, shift } from '~~/server/database/schemas'
 import { addBusinessDays, formatBusinessDate, getCalendarDate, parseBusinessDate, resolveBusinessDateFromInstant } from '~~/shared/utils/attendance-date'
 import { trackServerEvent } from '../../../modules/error-reporting/runtime/server/utils/error-reporting'
+import { validateAttendanceShiftDefinition, validateAttendanceShiftType } from '../../utils/attendance-shift-validation'
 import { useDb } from '../../utils/db'
 
 export default defineEventHandler(async (event) => {
@@ -22,9 +23,7 @@ export default defineEventHandler(async (event) => {
     geofenceName?: string
   }
   if (!bodyTimeZone) throw createError({ statusCode: 400, statusMessage: 'timeZone required' })
-  if (shiftType !== undefined && shiftType !== 'harian' && shiftType !== 'bantuan') {
-    throw createError({ statusCode: 400, statusMessage: 'Invalid shiftType' })
-  }
+  validateAttendanceShiftType(shiftType)
 
   const db = useDb()
   const userId = session.user.id
@@ -42,8 +41,7 @@ export default defineEventHandler(async (event) => {
     await tx.execute(sql`SELECT pg_advisory_xact_lock(hashtextextended(${userId}, 0))`)
 
     const shiftDef = shiftCode ? (await tx.select().from(shift).where(eq(shift.code, shiftCode)).limit(1))[0] : undefined
-    if (shiftCode && !shiftDef) throw createError({ statusCode: 400, statusMessage: 'Invalid shiftCode' })
-    if (shiftDef && !shiftDef.active) throw createError({ statusCode: 400, statusMessage: 'Shift is inactive' })
+    validateAttendanceShiftDefinition(shiftCode, shiftDef)
 
     const targetDate = shiftDef
       ? formatBusinessDate(resolveBusinessDateFromInstant(now, { start: shiftDef.start, end: shiftDef.end }, bodyTimeZone))
