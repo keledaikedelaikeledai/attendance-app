b97948e758bc3f8f660479cb1e7954e0e5440cf9
keledaikedelaikeledai <keledaikedelaikeledai@gmail.com>
2026-09-17 12:14:28 +0700

    revert: keep integrity migration on feature branch

--- server/database/migrations/0006_attendance_log_constraints.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "attendance_log"
-  ADD CONSTRAINT "attendance_log_type_check" CHECK ("type" IN ('clock-in', 'clock-out'));
-
-ALTER TABLE "attendance_log"
-  ADD CONSTRAINT "attendance_log_date_format_check" CHECK ("date" ~ '^\\d{4}-\\d{2}-\\d{2}$');
-
-ALTER TABLE "attendance_log"
-  ADD CONSTRAINT "attendance_log_shift_type_check" CHECK ("shift_type" IS NULL OR "shift_type" IN ('harian', 'bantuan'));
